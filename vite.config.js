a11fd024ae938e9e7bf4310d8e628c27f49d5a27
Phil Plencner <philip_plencner@harvard.edu>
2026-01-15 11:55:23 -0500

    LTSVIEWER-395 Adding JSX to Vite config.

--- vite.config.js
+++ vite.config.js
@@ -14,13 +14,14 @@ export default defineConfig({
   },
   esbuild: {
     loader: 'jsx',
-    include: /src\/.*\.js$/,
+    include: /src\/.*\.(js|jsx)$/,
     exclude: [],
   },
   optimizeDeps: {
     esbuildOptions: {
       loader: {
         '.js': 'jsx',
+        '.jsx': 'jsx',
       },
     },
   },
